e79ee1062ff2c8fcfd3e59dcff8de9e63c039720
Andres Ferrari <andresferrari590@gmail.com>
2024-11-14 11:31:48 -0300

    ...

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login as django_login
 
 def login(request):
-    return render(request, "usuarios/login.html")
+    formulario=AuthenticationForm
+    
+    if request.method == "POST":
+        formulario= AuthenticationForm(request, data=request.POST)
+        if formulario.is_valid():
+            nombre_de_usuario= formulario.cleaned_data.get("username")
+            contrasenia= formulario.cleaned_data.get("password")
+            
+            usuario= authenticate(username=nombre_de_usuario, password=contrasenia)
+            
+            django_login(request, usuario)
+            return redirect("inicio:inicio")
+    
+    return render(request, "usuarios/login.html", {"form": formulario})
